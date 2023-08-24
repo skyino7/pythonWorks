155c4608c1a5ef5f88940170a3d972edd93b361d
Skyino7 <christambassador12@gmail.com>
2023-08-24 23:42:01 +0000

    Python Test

---
+++ Tutorial/main.py
@@ -0,0 +1,73 @@
+# def get_choices():
+#     player_choice = input("Enter A Choice (Rock, Paper Scissors): ")
+#     computer_choice = "paper"
+#     choices = {"player": player_choice, "computer": computer_choice}
+#     return choices
+
+
+# response = get_choices()
+
+# print(response)
+
+# def add(a, b):
+
+# a = input("Type A Number: ")
+# b = input("Type Another Number: ")
+
+# sum = int(a) + int(b)
+
+# print("The Sum is: ", sum)
+
+# def func(str):
+#     return str[::-1]
+
+# rev = func("This is a reverse string")
+
+# print(rev)
+
+# cars = ['Toyota', 'BMW', 'Mercedes', 'VW']
+# cars.append('Jaguar')
+
+# print(cars)
+
+# cars = ['Toyota', 'BMW', 'Mercedes', 'VW']
+# newCars = cars.copy()
+
+# print(newCars)
+
+# cars = ['Toyota', 'BMW', 'Mercedes', 'VW', 'VW']
+# newCars = cars.count('VW')
+
+# print(newCars)
+
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+"""
+y = 10
+x = 20
+
+sum = x * y % 3
+print(sum)
+"""
+
+# x = "Life is Good!"
+# y = True
+# print(type(y))
+
+# x = 9
+# x = complex(x)
+# print(x)
+# print(type(x))
+
+# letter = "Life is about making adjustment along the way. Grow up"
+# count = len(letter)
+# print(count)
+
+# letter = "Life is about making adjustment along the way. Grow up"
+# x = letter[0:4]
+# print(x)
+
+letter = "        Life is about making adjustment along the way. Grow up      "
+trail = letter.strip()
+print(trail)
