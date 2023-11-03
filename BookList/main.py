47196a661fbb4a36cbeda68df0d3f966591e81fa
Skyino7 <christambassador12@gmail.com>
2023-11-03 23:52:47 +0000

    updated

--- BookList/main.py
+++ BookList/main.py
@@ -5,11 +5,12 @@ def main():
 
     booksList = []
 
-    infile = open("BooksList.txt", "r")
-    line = infile.readline()
-    while line:
-        booksList.append(line.rstrip("\n").split(","))
-        line = infile.readline()
+    # infile = open("BooksList.txt", "r")
+    # line = infile.readline()
+    # while line:
+    #     booksList.append(line.rstrip("\n").split(","))
+    #     line = infile.readline()
+    # infile.close()
 
     choice = 0
     while choice != 4:
@@ -19,3 +20,14 @@ def main():
         print("3) Display books")
         print("4) Quit")
         choice = int(input())
+
+        if choice == 1:
+            print("Adding a book...")
+            nBook = input("Enter the name of the book >>> ")
+            nAuthor = input("Enter the name of the Author >>> ")
+            nPages = input("Enter the number of pages >>> ")
+            booksList.append([nBook, nAuthor, nPages])
+
+
+if __name__ == "__main__":
+    main()
